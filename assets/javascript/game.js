ca2f3e905c143dfdad741fad1ff22ccb67416d59
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-02-13 23:15:40 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 //Set global variables
 var randomNumber = 0;
+previous = 0;
 var win = 0;
 var loss = 0;
-var sum = 0;
-previous = 0;
+
 
 //Create 4 diamonds and random result
 var beginNewGame = function () {
     $(".diamonds").empty();
     randomNumber = Math.floor(Math.random() * 101) + 19;
 
-    
     $("#random-number").html("Random Number: " + randomNumber);
+
     //Random numbers on each diamond
     for (var i = 0; i < 4; i++) {
 
@@ -21,7 +21,6 @@ var beginNewGame = function () {
         diamond.attr({
             "class": 'diamond',
             "random-data": randomNumberDiamond
-
         });
 
         $(".diamonds").append(diamond);
@@ -32,19 +31,19 @@ var beginNewGame = function () {
 beginNewGame();
 //Create a function when click on diamond to add to previous number 
 
- 
+
 
 $(document).on('click', ".diamond", function () {
 
     var number = parseInt($(this).attr("random-data"));
     previous += number;
     $(".total-score").html("Your score: " + previous);
-
+    //If logic to count wins and losses. Also restart the adding numbers
     if (previous > randomNumber) {
         loss++;
         previous = 0;
         $("#loss").html("Loss: " + loss);
-        beginNewGame ();
+        beginNewGame();
 
     } else if (previous === randomNumber) {
         win++;
